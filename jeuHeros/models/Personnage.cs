effb2752739b8f93132a8e32328640950be0b152
David ROUMANET <golfy@free.fr>
2021-03-09 08:58:09 +0100

    séparation des fichiers de codes.

---
+++ jeuHeros/models/Personnage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeuHeros.models {
+    public class Personnage {
+        public String pseudo;
+        public Char sexe;           // permettre les autres (PAN, robots, extraterrestres, fantômes, etc.)
+        public int pointVie;
+        public int niveau;
+        public int experienceNiveau;    // pourcentage permettant le calcul de passage prochain niveau
+
+        public Personnage(String nom) {
+            this.pseudo = nom;
+        }
+        public Personnage(String nom, char sexe, int vie) {
+            this.pseudo = nom;
+            this.sexe = sexe;
+            this.pointVie = vie;
+        }
+    }
+    public class Guerrier : Personnage {
+        public int force;
+        public Guerrier(String Nom, int Force) : base(Nom) {
+            this.force = Force;
+        }
+    }
+}
