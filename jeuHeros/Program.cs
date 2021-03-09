7887027d1c65b05e96ca3e1232f57481b3826e14
David ROUMANET <golfy@free.fr>
2021-03-09 08:58:53 +0100

    Test avec les classes dans un fichier séparé

--- jeuHeros/Program.cs
+++ jeuHeros/Program.cs
@@ -12,34 +12,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using jeuHeros.models;          // importation des classes liées aux personnages
 
 namespace jeuHeros {
 
-    public class Personnage {
-        public String pseudo;
-        public Char sexe;           // permettre les autres (PAN, robots, extraterrestres, fantômes, etc.)
-        public int pointVie;
-        public int niveau;
-        public int experienceNiveau;    // pourcentage permettant le calcul de passage prochain niveau
-
-        public Personnage(String nom) {
-            this.pseudo = nom;
-        }
-        public Personnage(String nom, char sexe, int vie) {
-            this.pseudo = nom;
-            this.sexe = sexe;
-            this.pointVie = vie;
-        }
-    }
-    public class Guerrier : Personnage {
-        public int force;
-        public Guerrier(String Nom, int Force) : base(Nom) {
-            this.force = Force;
-        }
-    }
-
     class Program {
         static void Main(string[] args) {
+            Personnage human = new Personnage("Francis");
+            Guerrier barbare = new Guerrier("Conan", 150);
         }
     }
 }
